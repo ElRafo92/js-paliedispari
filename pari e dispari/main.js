8d1ff907095529fe77b559fa5145072cd2b56ed8
ElRafo92 <rafael.martinez@outlook.it>
2022-10-28 18:45:34 +0200

    finito pari o dispari

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -10,14 +10,44 @@ Dichiariamo chi ha vinto. */
 
 //console.log(userNum);
 
-let userNum = Number(prompt('inserisci un numero da 1 a 5:'));
 
-function randomNumber(num1, num2,) {
-   return Math.floor(Math.random() * num2 + num1);
+ 
+ 
+ function EvenOrOdd(risultato) {
+     if (risultato % 2 === 0) {
+         return console.log('la somma è pari')
+     } else {
+         return console.log('la somma è dispari');
+     }
+ };
+
+let userNum = Number(prompt('inserisci un numero da 1 a 5:'));
+console.log(userNum);
+
+if (userNum > 5 || isNaN(userNum)) {
+    alert('inserisci un numero da 1 a 5');
+    location.reload(window)
 }
 
-let pcNum = randomNumber(1, 5);
+function randomNumber(num1,) {
+    return Math.floor(Math.random() * num1 + 1);
+ };
+
+let pcNum = randomNumber(5);
 console.log(pcNum);
+
+function EvenOrOdd(risultato) {
+    if (risultato % 2 === 0) {
+        return console.log('la somma è pari')
+    } else {
+        return console.log('la somma è dispari');
+    }
+};
 
 let sum = pcNum + userNum;
 console.log(sum);
+
+let pariOdispari = EvenOrOdd(sum);
+    console.log(pariOdispari);
+
+
