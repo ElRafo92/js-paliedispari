80b98699d60764648c0e57b76f0ff1a619feb083
ElRafo92 <rafael.martinez@outlook.it>
2022-10-29 17:34:58 +0200

    finito palindromo

--- palindroma/main.js
+++ palindroma/main.js
@@ -6,35 +6,41 @@ Creare una funzione per capire se la parola inserita è palindroma */
 let parolaInput = prompt('inserisci una parola: ');
 console.log(parolaInput);
 
+
+function palindromoDetector(element) {
+
 let straight = [];
-     for (let index = 0; index < parolaInput.length; index ++) {
-        console.log(parolaInput[index]);
-        straight.push(parolaInput[index]);
-    }
+     for (let index = 0; index < element.length; index ++) {
+        console.log(element[index]);
+        straight.push(element[index]);
+    };
     
 
 let contr = [];
-    for (let index = parolaInput.length - 1; index >= 0; index --) {
-        console.log(parolaInput[index]);
-        contr.push(parolaInput[index]);
-    }
+    for (let index = element.length - 1; index >= 0; index --) {
+        console.log(element[index]);
+        contr.push(element[index]);
+    };
     
 let bool = true;
 
-for (i = 0; i < parolaInput.length; i ++) {
+for (i = 0; i < element.length; i ++) {
 
-if (contr[i] == straight[i]) {
+    if (contr[i] == straight[i]) {
 
     bool = true;
-}
-else {
+    }
+    else {
     bool = false;
-}}
+    }
+};
 
 if (bool == true) {
-    alert('palindromo');
+   return alert('palindromo');
 } 
 else {
-    alert('cccc');
+   return alert('try another word');
+};
 }
 
+console.log(palindromoDetector(parolaInput));
